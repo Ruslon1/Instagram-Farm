4fcc67b4c5cacff9dd9b4b6a3f1a689cecae4a48
Ruslon1 <rrakhimov06@gmail.com>
2025-05-19 12:53:46 +0500

    PostgreSQL DB

--- modules/database.py
+++ modules/database.py
@@ -1,7 +1,25 @@
-import sqlite3
+import psycopg2
+
+def get_database_connection():
+    return psycopg2.connect(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
+    )
 
 def load_accounts_and_videos():
-    db = sqlite3.connect("database.sqlite")
-    accounts = db.execute("SELECT * FROM Accounts").fetchall()
-    videos = db.execute("SELECT * FROM Videos").fetchall()
+    connection = get_database_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM Accounts")
+    accounts = cursor.fetchall()
+
+    cursor.execute("SELECT * FROM Videos")
+    videos = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
     return accounts, videos
