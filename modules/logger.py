f13a046308948a8c213341441b43501cbe1dcd73
Ruslon1 <rrakhimov06@gmail.com>
2025-05-23 17:04:33 +0500

    Fix logging

--- modules/logger.py
+++ modules/logger.py
@@ -1,8 +1,8 @@
 from telegram import Bot
 
-def telegram_notify(token, chat_id, message):
+async def telegram_notify(token, chat_id, message):
     try:
         bot = Bot(token=token)
-        bot.send_message(chat_id=chat_id, text=message)
+        await bot.send_message(chat_id=chat_id, text=message)
     except Exception as e:
         print(f"Telegram notification error: {e}")
